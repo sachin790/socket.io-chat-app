ab36f0af551b790ca4f60a908196ffa8a241cbb9
unknown <bhardwaj.sachin44@gmail.com>
2018-07-26 10:55:16 +0530

    socket.io chat

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var socket = require('socket.io');
+
+
+//App Setup 
+var app = express();
+var server = app.listen(4000,function(){
+	
+	console.log("Listening to requets on port 4000");
+	
+});
+
+// Static files 
+app.use(express.static('public'));
+
+//Socket Setup 
+var io = socket(server);
+
+io.on('connection',function(socket){
+	
+	console.log("made socket connection",socket.id);
+	//Handle chat Event
+	socket.on('chat',function(data){
+		io.sockets.emit('chat',data);
+	});
+	
+socket.on('typing',function(data){
+	
+	socket.broadcast.emit('typing',data);
+	
+});	
+	
+});
